4e760a977951e4e2fabb3df1105a0f6464467c5c
Valentin <valentin@elephantslab.com>
2025-09-05 14:28:53 +0300

    setup precaching for main site files

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,12 @@
 self.addEventListener("install", (event) => {
     console.log("[Service Worker] Installing Service Worker... ", event);
+    event.waitUntil(
+        // waits until cache is not open, otherwise it would go to activation phase
+        caches.open("static").then(function (cache) {
+            console.log("[Service Worker] Precaching App Shell");
+            cache.addAll(["/", "/index.html", "/src/js/app.js", "/src/js/feed.js", "/src/js/material.min.js", "/src/css/app.css", "/src/css/feed.css", "/src/images/main-image.jpg", "https://fonts.googleapis.com/icon?family=Material+Icons", "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css", "https://fonts.googleapis.com/css?family=Roboto:400,700"]);
+        })
+    );
 });
 self.addEventListener("activate", (event) => {
     console.log("[Service Worker] Activating Service Worker... ", event);
@@ -7,5 +14,13 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (e) => {
-    e.respondWith(fetch(e.request));
+    e.respondWith(
+        caches.match(e.request).then((res) => {
+            if (res) {
+                return res;
+            } else {
+                return fetch(e.request);
+            }
+        })
+    );
 });
