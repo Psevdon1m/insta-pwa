ce15419c9877495b5f217140c4419f8ea42ed0da
Valentin <valentin@elephantslab.com>
2025-09-08 16:01:04 +0300

    add fallback page if cached one is not available

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_STATIC_NAME = "static-v1";
+var CACHE_STATIC_NAME = "static-v2";
 var CACHE_DYNAMIC_NAME = "dynamic-v1";
 self.addEventListener("install", (event) => {
     console.log("[Service Worker] Installing Service Worker... ", event);
@@ -7,7 +7,7 @@ self.addEventListener("install", (event) => {
         caches.open(CACHE_STATIC_NAME).then(function (cache) {
             //update static version on new releases
             console.log("[Service Worker] Precaching App Shell");
-            cache.addAll(["/", "/index.html", "/src/js/app.js", "/src/js/feed.js", "/src/js/material.min.js", "/src/css/app.css", "/src/css/feed.css", "/src/images/main-image.jpg", "https://fonts.googleapis.com/icon?family=Material+Icons", "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css", "https://fonts.googleapis.com/css?family=Roboto:400,700"]);
+            cache.addAll(["/", "/index.html", "/offline.html", "/src/js/app.js", "/src/js/feed.js", "/src/js/material.min.js", "/src/css/app.css", "/src/css/feed.css", "/src/images/main-image.jpg", "https://fonts.googleapis.com/icon?family=Material+Icons", "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css", "https://fonts.googleapis.com/css?family=Roboto:400,700"]);
         })
     );
 });
@@ -42,7 +42,11 @@ self.addEventListener("fetch", (e) => {
                             return res;
                         });
                     })
-                    .catch((e) => console.log(e));
+                    .catch((e) => {
+                        return caches.open(CACHE_STATIC_NAME).then((cache) => {
+                            return cache.match("/offline.html");
+                        });
+                    });
             }
         })
     );
