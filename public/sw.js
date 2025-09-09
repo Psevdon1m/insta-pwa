720d4c7585e3d7e563940d2ffdac3411e6228f59
Valentin <valentin@elephantslab.com>
2025-09-09 13:20:45 +0300

    add remove cache logic before save new cache

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,17 @@
 var CACHE_STATIC_NAME = "static-v11121";
 var CACHE_DYNAMIC_NAME = "dynamic-v11";
 var STATIC_FILES = ["/", "/index.html", "/offline.html", "/src/js/app.js", "/src/js/feed.js", "/src/js/material.min.js", "/src/css/app.css", "/src/css/feed.css", "/src/images/main-image.jpg", "https://fonts.googleapis.com/icon?family=Material+Icons", "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css", "https://fonts.googleapis.com/css?family=Roboto:400,700"];
+
+function trimCache(cacheName, maxItems) {
+    caches.open(cacheName).then((cache) => {
+        return cache.keys().then((keys) => {
+            if (keys.length > maxItems) {
+                //recurise call
+                cache.delete(keys[0]).then(trimCache(cacheName, maxItems));
+            }
+        });
+    });
+}
 self.addEventListener("install", (event) => {
     console.log("[Service Worker] Installing Service Worker... ", event);
     event.waitUntil(
@@ -99,13 +110,13 @@ function isInArray(string, array) {
 
 self.addEventListener("fetch", (e) => {
     var url = "https://rickandmortyapi.com/api";
-    var staticAssets = [];
 
     if (e.request.url.indexOf(url) > -1) {
         //cache then network
         e.respondWith(
             caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
                 return fetch(e.request).then((res) => {
+                    trimCache(CACHE_DYNAMIC_NAME, 25);
                     cache.put(e.request, res.clone());
                     return res;
                 });
@@ -123,6 +134,7 @@ self.addEventListener("fetch", (e) => {
                     return fetch(e.request)
                         .then((res) => {
                             return caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
+                                trimCache(CACHE_DYNAMIC_NAME, 25);
                                 cache.put(e.request.url, res.clone()); //res can be consumed once, therefore return will be empty, so we need to call clone method
                                 return res;
                             });
