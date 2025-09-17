9bc4d4254a9a40422030849f94907d2063da15d9
Valentin <valentin@elephantslab.com>
2025-09-17 14:30:06 +0300

    sending push messages on adding posts

--- backend/src/routes/addpost.js
+++ backend/src/routes/addpost.js
@@ -1,5 +1,6 @@
 import express from "express";
 import fetch from "node-fetch";
+import webpush from "web-push";
 const router = express.Router();
 router.post("/api/add-post", async (req, res) => {
     console.log("[Backend]: Request received");
@@ -11,6 +12,24 @@ router.post("/api/add-post", async (req, res) => {
     console.log("[Backend]: Firebase called");
     if (result.ok) {
         console.log("[Backend]: Result is OK");
+        webpush.setVapidDetails("mailto:valentine.oleynik@gmail.com", process.env.vapid_pub_key, process.env.vapid_private_key);
+        let subs_result = await fetch(process.env.firebase_url + "/subscriptions.json");
+        let subs = await subs_result.json();
+        Object.values(subs).forEach((sub) => {
+            let pushConfig = {
+                endpoint: sub.endpoint,
+                keys: {
+                    auth: sub.keys.auth,
+                    p256dh: sub.keys.p256dh,
+                },
+            };
+            try {
+                webpush.sendNotification(pushConfig, JSON.stringify({ title: "New Post", content: "New Post Added!" }));
+            } catch (error) {
+                console.log("Error sending pushes: ", error);
+            }
+        });
+
         res.status(201).send({ message: "Post has been stored" });
     }
 });
